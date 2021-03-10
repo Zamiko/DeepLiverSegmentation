5c99518bdcf3d611b027d55874b4b669d7554f6e
cat-castle-99 <56051215+cat-castle-99@users.noreply.github.com>
2021-03-10 00:37:14 -0800

    Delete webapp/embedded/webMain/Viewers directory
    too tired to move all the files at the moment

--- webapp/embedded/webMain/Viewers/CONTRIBUTING.md
+++
@@ -1,1 +0,0 @@
-See our contributing guidelines at [`https://docs.ohif.org`](https://docs.ohif.org/development/contributing.html)
--- webapp/embedded/webMain/Viewers/eslintAliasesResolver.js
+++
@@ -1,8 +0,0 @@
-module.exports.interfaceVersion = 2;
-
-module.exports.resolve = (source, file, aliases) => {
-  if (aliases[source]) {
-    return { found: true, path: aliases[source] };
-  }
-  return { found: false };
-};
