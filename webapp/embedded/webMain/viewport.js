3dd9cd6caa3b9c0e26848412bd4e53ddb47072e4
cat-castle-99 <56051215+cat-castle-99@users.noreply.github.com>
2021-04-06 16:59:37 -0700

    Create viewport.js

---
+++ webapp/embedded/webMain/viewport.js
@@ -0,0 +1,28 @@
+"use strict";
+//TODO: this is where we'd change the image data, I believe
+var imageIds = [];
+for (var i = 1; i < 72; i++) {
+  imageIds.push(
+    "dicomweb://" +
+      window.location.host +
+      "/DICOM_Data/C3N-00198/08-31-2009-CT ABDOMEN W IV CONTRAST-36291/6.000000-AbdPANC 2.0 B31f-92277/1-" +
+      i +
+      ".dcm"
+  );
+}
+var exampleData = {
+  stack: {
+    currentImageIdIndex: 0,
+    imageIds
+  }
+};
+
+var CornerstoneViewport = window["react-cornerstone-viewport"];
+var props = {
+  viewportData: exampleData,
+  cornerstone,
+  cornerstoneTools
+};
+var app = React.createElement(CornerstoneViewport, props, null);
+
+ReactDOM.render(app, document.getElementById("cornerstoneViewport"));
