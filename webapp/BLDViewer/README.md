0432c8b82f487330f0bf59b23ebb58c41657684a
cat-castle-99 <56051215+cat-castle-99@users.noreply.github.com>
2021-04-12 22:21:17 -0700

    Updated server-side files
    Please note, these files will not work with Firebase. Also, the requests to the data store don't actually work (oops). I've figured that the issue is the permissions/authentication, which needed to be changed anyway so we can keep the storage bucket private. At the time of this commit, honestly just too tired to keep working on it.

---
+++ webapp/BLDViewer/README.md
@@ -0,0 +1,8 @@
+# README.md
+
+This is a node.js app with dependencies:
+    express
+    googleapis
+
+which can both be installed with npm install
+
--- webapp/BLDViewer/server.js
+++ webapp/BLDViewer/server.js
@@ -1,107 +1,123 @@
-// server.js
-// where your node app starts
-
 // init project
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const fs = require("fs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// init sqlite db
-const dbFile = "./.data/sqlite.db";
-const exists = fs.existsSync(dbFile);
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database(dbFile);
-
-//COPIED THE EXAMPLE FOR FUTURE USE WITH THE LOGIN DATABASE, we'll need to include cookies if we're creating our own database though
-// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
-/*db.serialize(() => {
-  if (!exists) {
-    db.run(
-      "CREATE TABLE Dreams (id INTEGER PRIMARY KEY AUTOINCREMENT, dream TEXT)"
-    );
-    console.log("New table Dreams created!");
-
-    // insert default dreams
-    db.serialize(() => {
-      db.run(
-        'INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")'
-      );
-    });
-  } else {
-    console.log('Database "Dreams" ready to go!');
-    db.each("SELECT * from Dreams", (err, row) => {
-      if (row) {
-        console.log(`record: ${row.dream}`);
-      }
-    });
-  }
-});*/
+app.use(express.static(`${__dirname}/webMain`));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.sendFile(`${__dirname}/webMain/index.html`);
 });
 
-/*
-// endpoint to get all the dreams in the database
-app.get("/getDreams", (request, response) => {
-  db.all("SELECT * from Dreams", (err, rows) => {
-    response.send(JSON.stringify(rows));
+//This code is all for:
+//importing DICOM instances from the cloud storage to the DICOM store, which we are using for more permanent storage
+//exporting DICOM instances from the DICOM store to the cloud storage, this will allow us to access DICOM studies sent from a PACS
+const { google } = require("googleapis");
+const healthcare = google.healthcare("v1");
+//const sleep = require("../sleep");--this dependency was causing issues
+const sleep = require('util').promisify(setTimeout);
+
+//Variables needed
+//Should we also move auth here?
+const cloudRegion = "us-west2";
+const projectId = "liversegmentationwebapp";
+const datasetId = "DICOM_data";
+//TODO(?): I was planning we have two stores, one for the data Roger's given us, and then when we submit the project it should be a new/clean datastore? or we can just not bother lol
+const dicomStoreId = "testing_data";
+//FIXME: uhh idk if we should give this a directory
+const gcsUri = "liver_segmentation";
+const name = `projects/${projectId}/locations/${cloudRegion}/datasets/${datasetId}/dicomStores/${dicomStoreId}`;
+
+const importDicomInstance = async () => {
+  const auth = await google.auth.getClient({
+    scopes: ["https://www.googleapis.com/auth/cloud-platform"]
   });
-});
+  google.options({ auth });
 
-// endpoint to add a dream to the database
-app.post("/addDream", (request, response) => {
-  console.log(`add to dreams ${request.body.dream}`);
-
-  // DISALLOW_WRITE is an ENV variable that gets reset for new projects
-  // so they can write to the database
-  if (!process.env.DISALLOW_WRITE) {
-    const cleansedDream = cleanseString(request.body.dream);
-    db.run(`INSERT INTO Dreams (dream) VALUES (?)`, cleansedDream, error => {
-      if (error) {
-        response.send({ message: "error!" });
-      } else {
-        response.send({ message: "success" });
+  const request = {
+    name,
+    resource: {
+      // The location of the DICOM instances in Cloud Storage
+      gcsSource: {
+        uri: `gs://${gcsUri}`
       }
-    });
-  }
-});
+    }
+  };
 
-// endpoint to clear dreams from the database
-app.get("/clearDreams", (request, response) => {
-  // DISALLOW_WRITE is an ENV variable that gets reset for new projects so you can write to the database
-  if (!process.env.DISALLOW_WRITE) {
-    db.each(
-      "SELECT * from Dreams",
-      (err, row) => {
-        console.log("row", row);
-        db.run(`DELETE FROM Dreams WHERE ID=?`, row.id, error => {
-          if (row) {
-            console.log(`deleted row ${row.id}`);
-          }
-        });
-      },
-      err => {
-        if (err) {
-          response.send({ message: "error!" });
-        } else {
-          response.send({ message: "success" });
-        }
-      }
+  const operation = await healthcare.projects.locations.datasets.dicomStores.import(
+    request
+  );
+  const operationName = operation.data.name;
+
+  const operationRequest = { name: operationName };
+
+  // Wait fifteen seconds for the LRO to finish.
+  await sleep(15000);
+
+  // Check the LRO's status
+  const operationStatus = await healthcare.projects.locations.datasets.operations.get(
+    operationRequest
+  );
+
+  const { data } = operationStatus;
+
+  if (data.error === undefined) {
+    console.log("Successfully imported DICOM instances");
+  } else {
+    console.log("Encountered errors. Sample error:");
+    console.log(
+      "Resource on which error occured:",
+      data.error.details[0]["sampleErrors"][0]["resource"]
+    );
+    console.log(
+      "Error code:",
+      data.error.details[0]["sampleErrors"][0]["error"]["code"]
+    );
+    console.log(
+      "Error message:",
+      data.error.details[0]["sampleErrors"][0]["error"]["message"]
     );
   }
+};
+
+//I believe after calling this command there will no longer be any files stored in the cloud storage directory we're using
+app.get("/unloadDataStore", (request, response) => {
+  importDicomInstance();
+  console.log("unloaded Data Store");
 });
-*/
+
+const exportDicomInstance = async () => {
+  //FIXME: need to change this so that it doesn't request permission? maybe get it to use a service account
+  const auth = await google.auth.getClient({
+    scopes: ["https://www.googleapis.com/auth/cloud-platform"]
+  });
+  google.options({ auth });
+  const request = {
+    name,
+    resource: {
+      gcsDestination: {
+        // The destination location of the DICOM instances in Cloud Storage
+        uriPrefix: `gs://${gcsUri}`,
+        // The format to use for the output files, per the MIME types supported in the DICOM spec
+        mimeType: "application/dicom"
+      }
+    }
+  };
+
+  await healthcare.projects.locations.datasets.dicomStores.export(request);
+  console.log(`Exported DICOM instances to ${gcsUri}`);
+};
+//TODO: there is (in beta) a filter file suggestion that allows us to only import a few files (ie a study) rather than everything in the Datastore this might be good to look at
+app.get("/loadDataStore", (request, response) => {
+  exportDicomInstance();
+  console.log("loaded Data Store");
+});
+//End of data store manipulation
 
 //check for invalid function calls
 app.all("*", function(request, response) {
