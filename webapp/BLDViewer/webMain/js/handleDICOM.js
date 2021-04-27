470be97127412238fc22f22ce36f2c3b971c9993
cat-castle-99 <56051215+cat-castle-99@users.noreply.github.com>
2021-04-26 18:41:41 -0700

    Updating handleDicom to reflect dicomweb commands

--- webapp/BLDViewer/webMain/js/handleDICOM.js
+++ webapp/BLDViewer/webMain/js/handleDICOM.js
@@ -1,18 +1,13 @@
-// Upload Image using a post request
-// Called by the event listener that is waiting for a file to be chosen
-//not using this at the moment
-function uploadFile() {
-    // get the file chosen by the file dialog control
-    const selectedFile = document.getElementById("fileChooser").files[0];
-    // store it in a FormData object
-    const formData = new FormData();
-    // name of field, the file itself, and its name
-    formData.append("newImage", selectedFile, selectedFile.name);
-  
+//we'll have to change this later
+const studyUrl = "49591";
+
+
+function store() {
+    //we're going to want to store the url of the study to store in the request we send
+
     // build a browser-style HTTP request data structure
     const xHTTPreq = new XMLHttpRequest();
-    // it will be a POST request, the URL will this page's URL+"/upload"
-    xHTTPreq.open("POST", "/uploadDICOM", true);
+    xHTTPreq.open("GET", "/store", true);
     // callback function executed when the HTTP response comes back
     xHTTPreq.onloadend = function(e) {
       // Get the server's response body
@@ -20,31 +15,19 @@ function uploadFile() {
     };
   
     // actually send the request
-    xHTTPreq.send(formData);
+    xHTTPreq.send();
   }
-function importDataStoretoBucket(){
+function retrieve(){
+    //we're going to want to store the url of the study to retrieve, I think. Not yet got that working.
     const xHTTPreq = new XMLHttpRequest();
-    //this will be a Get request--maybe should change to a POST request when we implement study selection? unnecessary though, I think
-    xHTTPreq.open("GET", "/loadDataStore");
+    xHTTPreq.open("POST", "/retrieve");
     xHTTPreq.addEventListener("load", function() {
         if (xHTTPreq.status != 200) {
           console.log(xHTTPreq.responseText);
         }
       });
-      xHTTPreq.send();
-}
-function exportDataStoretoBucket(){
-    const xHTTPreq = new XMLHttpRequest();
-    //this will be a Get request--maybe should change to a POST request when we implement study selection? unnecessary though, I think
-    xHTTPreq.open("GET", "/unloadDataStore");
-    xHTTPreq.addEventListener("load", function() {
-        if (xHTTPreq.status != 200) {
-          console.log(xHTTPreq.responseText);
-        }
-      });
-      xHTTPreq.send();
+      xHTTPreq.send(studyUrl);
 }
 // Add event listener to the file input element
-document.getElementById("uploadDICOM").addEventListener("change", uploadFile);
-document.getElementById("loadDICOM").addEventListener("click", importDataStoretoBucket);
-document.getElementById("unloadDICOM").addEventListener("click", exportDataStoretoBucket);
+document.getElementById("saveDICOM").addEventListener("click", store());
+document.getElementById("loadDICOM").addEventListener("click", retrieve);
