ff77c40eb0d22c73886d0b09dd2de36948289784
cat-castle-99 <56051215+cat-castle-99@users.noreply.github.com>
2021-04-26 18:43:54 -0700

    Implemented Dicomweb store

--- webapp/BLDViewer/README.md
+++ webapp/BLDViewer/README.md
@@ -9,19 +9,20 @@ which can both be installed with npm install
 The dicomweb commands:  
 How I believe these should work are as follows:  
 - Upon logging in (yet to be implemented) the user will be prompted to choose a study (the study selection command--yet to be implemented + tested)
-- Upon selecting a study, we will store(command yet to be implemented + tested) any study currently held locally (if there is any study to store) and delete the local copy(if storing doesn't already remove it)
+- Upon selecting a study, we will store any study currently held locally (if there is any study to store) and delete the local copy  
+    - alternatively perhaps if we can check for cookies, we can provide an idle prompt to save before re-requesting the user log in again. If they choose to save, then we store the study, otherwise we don't. In any case we can delete the local copy
 - When the local location of our dicom files is empty, we will retrieve(yet to be implemented + tested) the chosen study
-- After loading this study, we then must display it (this will involve changing the current implementation of file loading only slightly)
+- After loading this study, we then must display it (this will involve changing the current implementation of file loading only slightly--it will still be from a local address)
 - When the user chooses to load a segmentation:  
     if there is already segmentation data, load it
     if there is no segmentation data, pass the dicom files to the machine learning model so the segmentation masks are produced and then loaded
 - When the user chooses to save a segmentation, we will save it to the local study. If storing doesn't remove the local copy, we can also send a store command so that the new changes are saved in our dataset  
-    I don't really know what we'd do otherwise: if we'd just store and retrieve or wait until the user presses a "done" button  
+    - I don't really know what we'd do otherwise: if we'd just store and retrieve or wait until the user presses a "done" button  
     - on this note should we also provide an abort button that allows the user to not save anything to the dataset?
-        -perhaps this could simply be a "back" button and will return user to the study selection screen
+        - perhaps this could simply be a "back" button and will return user to the study selection screen
 
 The order in which we should implement and test the dicomweb commands is:  
-store (as the dataset is currently empty)  
+store-Done
 retrieve  
 search  
 I provided the code for the deletion of a study in the dataset, but I don't know that it would be necessary
--- webapp/BLDViewer/server.js
+++ webapp/BLDViewer/server.js
@@ -15,7 +15,6 @@ app.use(express.static("webMain"));
 //was trying to implement all this via a python script--however have to figure out authentication for that
 const {google} = require('googleapis');
 const healthcare = google.healthcare('v1');
-const fs = require('fs');
 const util = require('util');
 const writeFile = util.promisify(fs.writeFile);
 // When specifying the output file, use an extension like ".multipart."
@@ -28,38 +27,86 @@ const projectId = 'liversegmentationwebapp';
 const datasetId = 'DICOM_data';
 const dicomStoreId = 'testing_data';
 
-const dicomWebStoreInstance = async () => {
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(`${__dirname}/webMain/index.html`);
+});
+
+
+//FIXME: for all three of these functions I may need to directly write the async fucntion here instead of writing it elsewhere and calling it here
+app.get("/store", async (req, res) => {
+    const auth = await google.auth.getClient({
+      scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+    });
+    google.options({
+      auth,
+      headers: {
+        'Content-Type': 'application/dicom',
+        Accept: 'application/dicom+json',
+      },
+    });
+    //Ideally I think we should aim to only have one study held locally (per user???) so we can just link from that location 
+    fileFolder = "webMain/DICOM_Data/C3N-00198/08-31-2009-CT ABDOMEN W IV CONTRAST-36291/6.000000-AbdPANC 2.0 B31f-92277"
+    fs.readdir(fileFolder, async (err, files) => {
+      if (err) {
+        console.error("Could not list the directory.", err);
+        process.exit(1);
+      }
+    
+      files.forEach(async(file, index) => {
+        const parent = `projects/${projectId}/locations/${cloudRegion}/datasets/${datasetId}/dicomStores/${dicomStoreId}`;
+        const dicomWebPath = 'studies';
+        // Use a stream because other types of reads overwrite the client's HTTP
+        // headers and cause storeInstances to fail.
+        filePath = fileFolder + "/" + file
+        const binaryData = fs.createReadStream(filePath);
+        const request = {
+          parent,
+          dicomWebPath,
+          requestBody: binaryData,
+        };
+  
+        const instance = await healthcare.projects.locations.datasets.dicomStores.storeInstances(
+        request
+        );
+        console.log('Stored DICOM instance:\n', JSON.stringify(instance.data));
+      });
+    });
+  }
+);
+//FIXME: we can store the information of the study we want to retrieve in request and pass it to the function
+app.post("/retrieve", async (req, res) => {
   const auth = await google.auth.getClient({
     scopes: ['https://www.googleapis.com/auth/cloud-platform'],
   });
   google.options({
     auth,
     headers: {
-      'Content-Type': 'application/dicom',
-      Accept: 'application/dicom+json',
+      Accept: 'multipart/related; type=application/dicom; transfer-syntax=*',
     },
+    responseType: 'arraybuffer',
   });
-  //Ideally I think we should aim to only have one study held locally (per user???) so we can just link from that location
-  //const dcmFile = 
-  const parent = `projects/${projectId}/locations/${cloudRegion}/datasets/${datasetId}/dicomStores/${dicomStoreId}`;
-  const dicomWebPath = 'studies';
-  // Use a stream because other types of reads overwrite the client's HTTP
-  // headers and cause storeInstances to fail.
-  const binaryData = fs.createReadStream(dcmFile);
-  const request = {
-    parent,
-    dicomWebPath,
-    requestBody: binaryData,
-  };
 
-  const instance = await healthcare.projects.locations.datasets.dicomStores.storeInstances(
+  //studyUid will be stores in req
+  //possible FIXME: may have to convert body to string
+  studyUid = req.body;
+  const parent = `projects/${projectId}/locations/${cloudRegion}/datasets/${datasetId}/dicomStores/${dicomStoreId}`;
+  const dicomWebPath = `studies/${studyUid}`;
+  const request = {parent, dicomWebPath};
+
+  const study = await healthcare.projects.locations.datasets.dicomStores.studies.retrieveStudy(
     request
   );
-  console.log('Stored DICOM instance:\n', JSON.stringify(instance.data));
-};
 
+  const fileBytes = Buffer.from(study.data);
 
-const dicomWebSearchForInstances = async () => {
+  await writeFile(fileName, fileBytes);
+  console.log(
+    `Retrieved study and saved to ${fileName} in current directory`
+  );
+});
+
+app.get("/search", async (req, res) => {
   const auth = await google.auth.getClient({
     scopes: ['https://www.googleapis.com/auth/cloud-platform'],
   });
@@ -76,42 +123,13 @@ const dicomWebSearchForInstances = async () => {
     request
   );
   //FIXME: will have to send this data back to the frontend for display
+  //can do this through the response--I've done it before I believe, I jsut need to find the code
   console.log(`Found ${instances.data.length} instances:`);
   console.log(JSON.stringify(instances.data));
-};
+});
 
-
-
-const dicomWebRetrieveStudy = async (studyUid) => {
-  const auth = await google.auth.getClient({
-    scopes: ['https://www.googleapis.com/auth/cloud-platform'],
-  });
-  google.options({
-    auth,
-    headers: {
-      Accept: 'multipart/related; type=application/dicom; transfer-syntax=*',
-    },
-    responseType: 'arraybuffer',
-  });
-
-  const parent = `projects/${projectId}/locations/${cloudRegion}/datasets/${datasetId}/dicomStores/${dicomStoreId}`;
-  const dicomWebPath = `studies/${studyUid}`;
-  const request = {parent, dicomWebPath};
-
-  const study = await healthcare.projects.locations.datasets.dicomStores.studies.retrieveStudy(
-    request
-  );
-
-  const fileBytes = Buffer.from(study.data);
-
-  await writeFile(fileName, fileBytes);
-  console.log(
-    `Retrieved study and saved to ${fileName} in current directory`
-  );
-};
-
-
-const dicomWebDeleteStudy = async (studyUid) => {
+//need to get uID through the request
+app.get("/delete", async (req, res) => {
   const auth = await google.auth.getClient({
     scopes: ['https://www.googleapis.com/auth/cloud-platform'],
   });
@@ -125,34 +143,7 @@ const dicomWebDeleteStudy = async (studyUid) => {
     request
   );
   console.log('Deleted DICOM study');
-};
-
-//dicomWebDeleteStudy();
-
-
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(`${__dirname}/webMain/index.html`);
 });
-
-
-//FIXME: for all three of these functions I may need to directly write the async fucntion here instead of writing it elsewhere and calling it here
-app.get("/store", (request, response) => {
-  dicomWebStoreInstance();
-});
-
-app.get("/retrieve", (request, response) => {
-  //FIXME: we can store the information of the study we want to retrieve in request and pass it to the function
-  dicomWebRetrieveStudy();
-});
-
-app.get("/search", (request, response) => {
-  //FIXME: we'l need to pass the JSON information back through response
-  dicomWebSearchForInstances();
-});
-
-
 
 
 //check for invalid function calls
