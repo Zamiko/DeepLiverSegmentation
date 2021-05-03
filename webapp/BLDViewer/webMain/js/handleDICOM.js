70260c656a063e48979987d47fa5b9fe01e72a90
cat-castle-99 <56051215+cat-castle-99@users.noreply.github.com>
2021-05-03 16:47:48 -0700

    Finished parsing study stuff
    Now just need to implement them in a search bar, which I've begun implementing, but I thought this was a good checkpoint

--- webapp/BLDViewer/webMain/js/handleDICOM.js
+++ webapp/BLDViewer/webMain/js/handleDICOM.js
@@ -1,5 +1,13 @@
 //we'll have to change this later
 const studyUrl = "49591";
+let allStudies =[];
+
+
+//this will create a json object representing the study
+function Study(patientName, studyID) {
+  this.patientName = patientName;
+  this.studyID = studyID;
+}
 
 
 function store() {
@@ -42,9 +50,121 @@ function studySearch(){
         console.log(xHTTPreq.responseText);
       }
       else{
-        let StudyDataAr = JSON.parse(xhr.responseText);
-        let StudyData = postcardDataAr[0];
-        console.log("We received " + xhr.responseText + " from the server");
+        /*
+        [
+          {
+            "00080020":{"vr":"DA","Value":["20100510"]},
+            "00080030":{"vr":"TM","Value":["133352.906000"]},
+            "00080050":{"vr":"SH","Value":["00000001"]},
+            "00080090":{"vr":"PN"},
+            "00100010":{"vr":"PN","Value":[{"Alphabetic":"Patient^Anonymous"}]}, -- patient name
+            "00100020":{"vr":"LO","Value":["12345678"]},
+            "00100030":{"vr":"DA","Value":["19560324"]},
+            "00100040":{"vr":"CS","Value":["M"]},
+            "0020000D":{"vr":"UI","Value":["1.3.6.1.4.1.25403.52237031786.3872.20100510032220.1"]},--study id?
+            "00200010":{"vr":"SH","Value":["No Study ID"]}
+          },
+          {
+            "00080005":{"vr":"CS","Value":["ISO_IR 100"]},
+            "00080020":{"vr":"DA","Value":["20090831"]},
+            "00080030":{"vr":"TM","Value":["095948.599"]},
+            "00080050":{"vr":"SH","Value":["5471978513296937"]},
+            "00080090":{"vr":"PN"},
+            "00100010":{"vr":"PN","Value":[{"Alphabetic":"C3N-00198"}]}, -- patient name
+            "00100020":{"vr":"LO","Value":["C3N-00198"]},
+            "00100030":{"vr":"DA"},"00100040":{"vr":"CS","Value":["M"]},
+            "0020000D":{"vr":"UI","Value":["1.3.6.1.4.1.14519.5.2.1.7085.2626.822645453932810382886582736291"]}, -- study id
+            "00200010":{"vr":"SH"}
+          },
+          {
+            "00080005":{"vr":"CS","Value":["ISO_IR 100"]},
+            "00080020":{"vr":"DA","Value":["20030629"]},
+            "00080030":{"vr":"TM","Value":["081517.464000"]},
+            "00080050":{"vr":"SH"},
+            "00080090":{"vr":"PN"},
+            "00100010":{"vr":"PN","Value":[{"Alphabetic":"KiTS-00000"}]}, --patient name
+            "00100020":{"vr":"LO","Value":["KiTS-00000"]},
+            "00100030":{"vr":"DA"},
+            "00100040":{"vr":"CS","Value":["F"]},
+            "0020000D":{"vr":"UI","Value":["1.3.6.1.4.1.14519.5.2.1.6919.4624.135173370342136417423953641748"]}, -- study id
+            "00200010":{"vr":"SH"}}]
+          
+          */
+
+
+        let StudyDataString = xHTTPreq.responseText;
+        let StudyDataAr = StudyDataString.split("}")
+        //need to run through string until reaching "0010010"
+          //begin reading from 37 characters in to the next quotation mark
+            //discard if equal to Patient^Anonymous, otherwise, store in JSON as patient name
+          //studuid
+        // once we hit "}}" need to increase index so we're getting the next study
+        console.log("We received " + StudyDataAr + " from the server");
+        //Ignore first 11
+        patientName = '';
+        studyID =''; 
+        for(i = 12; i < StudyDataAr.length; i++){
+          pnIndex = StudyDataAr[i].indexOf('Alphabetic');
+          IDIndex = StudyDataAr[i].indexOf('UI\",\"Value');
+          if(pnIndex!=-1){
+            substr = StudyDataAr[i].split("\"");
+            patientName = substr[11];
+          }
+          if(IDIndex!=-1){
+            substr = StudyDataAr[i].split("\"");
+            studyID = substr[9];
+          }
+          if(StudyDataAr[i] === null || StudyDataAr[i] === ''){
+            //push a new JSON object to the array, with the patientName and studID
+            allStudies.push(new Study(patientName, studyID));
+          }
+        }
+        console.log("Found studies " + JSON.stringify(allStudies))
       }
     });
+  xHTTPreq.send();
 }
+
+
+
+//Search bar implementation
+const searchBar = document.getElementById('searchBar');
+let studies = [];
+
+searchBar.addEventListener('keyup', (e) => {
+  const searchString = e.target.value.toLowerCase();
+
+  const filteredSeries = studies.filter((series) => {
+      return (
+          series.name.toLowerCase().includes(searchString) ||
+          series.house.toLowerCase().includes(searchString)
+      );
+  });
+  displayCharacters(filteredSeries);
+});
+
+//change this to be what search studies does
+const loadSeries = async () => {
+  try {
+      studies = await res.json();
+      displayCharacters(hpCharacters);
+  } catch (err) {
+      console.error(err);
+  }
+};
+
+//will need to edit this to reflect the studyJSON
+const displayStudies = (series) => {
+  const htmlString = series
+      .map((series) => {
+          return `
+          <li class="character">
+              <h2>${character.name}</h2>
+              <p>House: ${character.house}</p>
+              <img src="${character.image}"></img>
+          </li>
+      `;
+      })
+      .join('');
+  charactersList.innerHTML = htmlString;
+};
