474cc28d15abecbfe7bd36802dbf3d89c4e0ae2b
Leo Martinez <59814405+LeoFabiann@users.noreply.github.com>
2021-05-18 20:15:42 -0700

    Added more attributes manually and bug fixes
    Manually added the source image sequence, procedure code sequence, and segment label attributes. Removed an unnecessary line of code mistakenly carried over from Google Colab.
    Added some warning messages for missing source attributes
    ToDo: Sort the pydicom input series by z position

--- machinelearning/SeriesToSeg.py
+++ machinelearning/SeriesToSeg.py
@@ -18,6 +18,8 @@ import matplotlib.pyplot as plt
 import math
 import cv2
 import os
+from pydicom.sequence import Sequence
+from pydicom.dataset import Dataset
 
 # Helper Functions
 # Change orientation of the input matrix
@@ -95,28 +97,29 @@ source_images = [
     for x in dicom_series_paths_new
 ]
 
+# TODO: Sort the Pydicom files by z position on image position patient attributes
+
 # Create and save a nifti volume of the DICOM series
 dicom2nifti.dicom_series_to_nifti(series_path, seriesNii_path)
 
 # Transpose the input series matrix to the correct orientation
 # TODO: Modify this so that it flips it to the correct orientation for any input orientation
-img_slice_transposed = matrixflip(img_slice,'v')
 
 # Load the series and the model
 model = load_model(model_path)
 niiFile = nib.load(seriesNii_path)
+imgTarget = nib.load(seriesNii).get_fdata()
 
 # Generate the segmentation of the DICOM series
 
 # Data Preprocessing
-imgTarget = nib.load(seriesNii).get_fdata()
 imgTarget = normalizeImageIntensityRange(imgTarget)
 imgTarget = np.transpose(imgTarget,(2,0,1))
 scaled_images = Scale_Images(imgTarget)
 
 # Predicting the segmentation masks
 mask_predicted = model.predict(scaled_images)
-mask_pred_argmax=np.argmax(mask_predicted, axis=3) #Convert the probability into a max value within the area
+mask_pred_argmax = np.argmax(mask_predicted, axis=3) #Convert the probability into a max value within the area
 scale_up_images = Scale_to_Original(mask_pred_argmax)
 scale_up_images = scale_up_images[:,:,:,0]
 scale_up_images_transposed = np.transpose(scale_up_images,(1,2,0))
@@ -125,9 +128,14 @@ scale_up_images_transposed = np.transpose(scale_up_images,(1,2,0))
 for i in range(0,scale_up_images_transposed.shape[2]):
   img = scale_up_images_transposed[:,:,i]
   scale_up_images_transposed[:,:,i] = matrixflip(img,'v')
-
+for i in range(0,scale_up_images_transposed.shape[2]):
+  img = scale_up_images_transposed[:,:,i]
+  scale_up_images_transposed[:,:,i] = matrixflip(img,'h'
+  
 # Convert the predicted volume to Niftii format such that it can be transformed to an SimpleITK image
- predImg = nib.Nifti1Image(scale_up_images_transposed, affine=np.eye(4))
+predImg = nib.Nifti1Image(scale_up_images_transposed, affine=np.eye(4))
+nib.save(predImg, predNii)
+segmentation = sitk.ReadImage(predNii)
 
 # Write a multi-class segmentation DICOM seg file
 template = pydicom_seg.template.from_dcmqi_metainfo(metainfo)
@@ -137,31 +145,60 @@ writer = pydicom_seg.MultiClassWriter(
     skip_empty_slices = False,
     skip_missing_segment=True,
 )
-
-# Write the DICOM Seg and modify some attributes Manually
 dcm = writer.write(segmentation, source_images)
+
+# Modify some DICOM metadata attributes manually
+
+# Shared Functional Group Sequence Attributes
 dcm.SharedFunctionalGroupsSequence[0].PlaneOrientationSequence[0].ImageOrientationPatient = source_images[0].ImageOrientationPatient
 dcm.SharedFunctionalGroupsSequence[0].PixelMeasuresSequence[0].SliceThickness = source_images[0].SliceThickness
 dcm.SharedFunctionalGroupsSequence[0].PixelMeasuresSequence[0].PixelSpacing = source_images[0].PixelSpacing
 
-# Include the referenced series sequence attributes manually
+# Referenced Series Sequence Atrributes
 dcm.ReferencedSeriesSequence = Sequence([Dataset()])
 temp = [Dataset() for x in range(len(source_images))] # Create list of datasets with length equal to amount of slices
 dcm.ReferencedSeriesSequence[0].ReferencedInstanceSequence = Sequence(temp)
 dcm.ReferencedSeriesSequence[0].SeriesInstanceUID = pydicom.uid.UID(source_images[0].SeriesInstanceUID)
-for i in range(len(source_images)):
+for i in range(len(source_images) - 1, -1, -1):
   classUID = source_images[i].SOPClassUID
   instanceUID = source_images[i].SOPInstanceUID
   dcm.ReferencedSeriesSequence[0].ReferencedInstanceSequence[i].ReferencedSOPClassUID = pydicom.uid.UID(classUID)
   dcm.ReferencedSeriesSequence[0].ReferencedInstanceSequence[i].ReferencedSOPInstanceUID = pydicom.uid.UID(instanceUID)
  
-# ClinicalTrialTImePoint Variables not always accessible in original DICOM series
+# ClinicalTrial Variables not always accessible in original DICOM series
 try:
   dcm.ClinicalTrialTimePointDescription = source_images[0].ClinicalTrialTimePointDescription
   dcm.ClinicalTrialTimePointID = source_images[0].ClinicalTrialTimePointID
   dcm.ClinicalTrialSeriesID = source_images[0].ClinicalTrialSeriesID # TODO: Connect this to "PatientName"
 except:
-  print("TODO: Log error")
+  print("Warning: Clinical Trial Attributes not present in referenced DICOM series")
+  
+# Manually add the Source Image Sequence
+for i in range(len(source_images)):
+  i_reverse = len(source_images) - i - 1 # ReferencedSOPInstanceIUD Item #0 Corresponds to the last image in the source images series
+  for j in range(i, i + 8 * len(source_images), len(source_images)):
+
+    dcm.PerFrameFunctionalGroupsSequence[j].DerivationImageSequence[0].SourceImageSequence = Sequence([Dataset()])
+    dcm.PerFrameFunctionalGroupsSequence[j].DerivationImageSequence[0].SourceImageSequence[0].ReferencedSOPClassUID = source_images[0].SOPClassUID
+    dcm.PerFrameFunctionalGroupsSequence[j].DerivationImageSequence[0].SourceImageSequence[0].ReferencedSOPInstanceUID = source_images[i_reverse].SOPInstanceUID
+    dcm.PerFrameFunctionalGroupsSequence[j].DerivationImageSequence[0].SourceImageSequence[0].PurposeOfReferenceCodeSequence = Sequence([Dataset()])
+    dcm.PerFrameFunctionalGroupsSequence[j].DerivationImageSequence[0].SourceImageSequence[0].PurposeOfReferenceCodeSequence[0].CodeValue = "121322"
+    dcm.PerFrameFunctionalGroupsSequence[j].DerivationImageSequence[0].SourceImageSequence[0].PurposeOfReferenceCodeSequence[0].CodingSchemeDesignator = "DCM"
+    dcm.PerFrameFunctionalGroupsSequence[j].DerivationImageSequence[0].SourceImageSequence[0].PurposeOfReferenceCodeSequence[0].CodeMeaning = "Source image for image processing operation"
+
+# Manually add the Procedure Code Sequence
+dcm.ProcedureCodeSequence = Sequence([Dataset()])
+try:
+  dcm.ProcedureCodeSequence[0] = source_images[0].ProcedureCodeSequence[0]
+except:
+  print("Warning: ProcedureCodeSequence not provided")
+  dcm.ProcedureCodeSequence[0].CodeValue = "121322"
+  dcm.ProcedureCodeSequence[0].CodingSchemeDesignator = "DCM"
+  dcm.ProcedureCodeSequence[0].CodeMeaning = "Segmentation"
+
+# Specify the Segment Label attribute
+for i in range(8):
+  dcm.SegmentSequence[i].SegmentLabel = "Liver"
 
 # Save the DICOM Segmentation to local directory
 dcm.save_as(DICOM_seg_path)
